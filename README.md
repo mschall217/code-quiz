19106645671df8609292c99082f8349d0c259c9a
Morgan Schall <morgan.allison.schall@gmail.com>
2021-02-20 18:32:27 -0500

    the quiz is working as a funcitoning quiz instead of alert but not complete yet

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # code-quiz
 This is a project to test our HTML/CSS/JS/WebAPI abilities. The goal is to build a website that tests the users knowledge of web development principles. It is a timed quiz that records the users highest scores based on how many they got right and how fast they completed the quiz. 
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Screenshot](#Screenshot)
+- [Support](#support)
+- [Contributing](#contributing)
+
+## Installation
+
+View my project at: 
+
+## Usage
+
+Replace the contents of `README.md` with your project's:
+
+- Name
+- Description
+- Installation instructions
+- Usage instructions
+- Support instructions
+- Contributing instructions
+- Licence
+
+Feel free to remove any sections that aren't applicable to your project.
+
+
+## Screenshot
+
+![]()
+
+## Support
+
+Please [open an issue](https://github.com/fraction/readme-boilerplate/issues/new) for support.
+
+## Contributing
+
+Please contribute using [Github Flow](https://guides.github.com/introduction/flow/). Create a branch, add commits, and [open a pull request](https://github.com/fraction/readme-boilerplate/compare/).
---
+++ js/main.js
@@ -0,0 +1,166 @@
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        answer: "Alerts",
+        choices: [
+            "Strings",
+            "Booleans",
+            "Alerts",
+            "Numbers"
+        ]
+    },
+    {
+        title: "The condition in an if/else statement is enclosed with:",
+        answer: "Parathentesis ()",
+        choices: [
+            "Quotes",
+            "Curly Brackets {}",
+            "Parathentesis ()",
+            "Square Brackets []"
+        ]
+    },
+    {
+        title: "Arrays in javascript can be used to store:",
+        answer: "All of the Above",
+        choices: [
+            "Numbers and Strings",
+            "Other Arrays",
+            "Booleans",
+            "All of the Above"
+        ]
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables: ",
+        answer: "Quotes",
+        choices: [
+            "Commas",
+            "Curly Brackets {}",
+            "Quotes",
+            "Parenthesis ()"
+        ]
+    },
+    {
+    title: "A very useful tool during development and debugging for printing content to the debugger: ",
+    answer: "console.log()",
+    choices: [
+        "Javascript",
+        "Terminal/Bash",
+        "For Loops",
+        "console.log()"
+    ]
+    },
+]
+
+// Declared variables
+var score = 0;
+var questionIndex = 0;
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector(".start");
+var questionsDiv = document.querySelector("#questionsDiv");
+var wrapper = document.querySelector("#wrapper");
+
+
+var secondsLeft = 76;
+var holdInterval = 0;
+var penalty = 10;
+var ulCreate = document.createElement("ul");
+
+//function to start timer when clicked
+timer.addEventListener("click", function () {
+    if (holdInterval === 0) {
+        holdInterval = setInterval(function () {
+            secondsLeft--;
+            currentTime.textContent = "Time: " + secondsLeft;
+
+            if (secondsLeft <= 0) {
+                clearInterval(holdInterval);
+                allDone();
+                currentTime.textContent = "Time's up!";
+            }
+        }, 1000);
+    }
+    render(questionIndex);
+});
+
+// Renders questions and choices to page: 
+function render(questionIndex) {
+    // Clears existing data 
+    questionsDiv.innerHTML = "";
+    ulCreate.innerHTML = "";
+    // For loops to loop through all questions in array
+    for (var i = 0; i < questions.length; i++) {
+        // Appends question title only
+        var userQuestion = questions[questionIndex].title;
+        var userChoices = questions[questionIndex].choices;
+        questionsDiv.textContent = userQuestion;
+    }
+    userChoices.forEach(function (newItem) {
+        var listItem = document.createElement("li");
+        listItem.textContent = newItem;
+        questionsDiv.appendChild(ulCreate);
+        ulCreate.appendChild(listItem);
+        listItem.addEventListener("click", (compare));
+    })
+}
+// function to compare choices with answer
+function compare(event) {
+    var element = event.target;
+
+    if (element.matches("li")) {
+
+        var createDiv = document.createElement("div");
+        createDiv.setAttribute("id", "createDiv");
+        // Correct condition 
+        if (element.textContent == questions[questionIndex].answer) {
+            score++;
+            createDiv.textContent = "Correct! The answer is:  " + questions[questionIndex].answer;
+            // Correct condition 
+        } else {
+            // Will deduct -5 seconds off secondsLeft for wrong answers
+            secondsLeft = secondsLeft - penalty;
+            createDiv.textContent = "Wrong! The correct answer is:  " + questions[questionIndex].answer;
+        }
+
+    }
+    // Question Index determines number question user is on
+    questionIndex++;
+
+    if (questionIndex >= questions.length) {
+        // All done will append last page with user stats
+        allDone();
+        createDiv.textContent = "End of quiz!" + " " + "You got  " + score + "/" + questions.length + " Correct!";
+    } else {
+        render(questionIndex);
+    }
+    questionsDiv.appendChild(createDiv);
+
+}
+// All done will append last page
+function allDone() {
+    questionsDiv.innerHTML = "";
+    currentTime.innerHTML = "";
+
+    // Heading:
+    var createH1 = document.createElement("h1");
+    createH1.setAttribute("id", "createH1");
+    createH1.textContent = "All Done!"
+
+    questionsDiv.appendChild(createH1);
+
+    // Paragraph
+    var createP = document.createElement("p");
+    createP.setAttribute("id", "createP");
+
+    questionsDiv.appendChild(createP);
+
+    // Calculates time remaining and replaces it with score
+    if (secondsLeft >= 0) {
+        var timeRemaining = secondsLeft;
+        var createP2 = document.createElement("p");
+        clearInterval(holdInterval);
+        createP.textContent = "Your final score is: " + timeRemaining;
+
+        questionsDiv.appendChild(createP2);
+    }
+};
+
