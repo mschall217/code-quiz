578098018ffab1f5a26601794474db17fe02abc0
Morgan Schall <morgan.allison.schall@gmail.com>
2021-06-23 20:56:59 -0400

    got choices appearing

--- js/main.js
+++ js/main.js
@@ -57,6 +57,30 @@ let currentQ = -1
 let time;
 let timer;
 
+const game = () => {
+    currentQ++;
+    //if current question being displayed is the last one then end the game
+    if(currentQ > questions.length -1){
+        // function to end the game 
+        return;
+    }
+    let questionEL = '<h3>' + questions[currentQ].title + '</h3>' 
+
+    for (var buttonLoop = 0; buttonLoop < questions[currentQ].choices.length; buttonLoop++) {
+        var buttonCode = "<button onclick=\"[ANS]\">[CHOICE]</button>"; 
+        buttonCode = buttonCode.replace("[CHOICE]", questions[currentQ].choices[buttonLoop]);
+        if (questions[currentQ].choices[buttonLoop] == questions[currentQ].answer) {
+            buttonCode = buttonCode.replace("[ANS]", "correct()");
+        } else {
+            buttonCode = buttonCode.replace("[ANS]", "incorrect()");
+        }
+        questionEL += buttonCode
+    }
+
+
+    document.querySelector('#questionsDiv').innerHTML = questionEL
+
+}
 
 const startQuiz = () =>{
     let time = 10;
@@ -73,22 +97,13 @@ const startQuiz = () =>{
             //function to end the game will go here. 
         }
     }, 1000)
+    
+    game();
 
 }
 
-
-const game = () => {
-    currentQ++;
-    //if current question being displayed is the last one then end the game
-    if(currentQ > questions.length -1){
-        // function to end the game 
-        return;
-    }
-    let questionEL = '<h3>' + questions[currentQ].title + '</h3>' 
+const startBtn = document.querySelector(".start")
+startBtn.addEventListener("click", startQuiz())
 
 
-    document.querySelector('#questionsDiv').innerHTML = questionEL
 
-}
-
-game();
